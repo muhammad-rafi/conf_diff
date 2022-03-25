f7cec779a3d3699f4c46411d0b7a94d3345af5e8
Muhammad Rafi <murafi@cisco.com>
2022-03-25 14:55:18 +0000

    poetry build

--- __init__.py
+++ __init__.py
@@ -6,4 +6,4 @@ from .html_format import html_style, difflib  # noqa: F401
 
 __author__ = """Muhammad Rafi"""
 # __version__ = importlib.metadata.version("conf-diff")
-__version__ = '0.1.0'
+__version__ = '0.2.0'
