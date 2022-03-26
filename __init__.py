303aa994c4c78cf8046e321724c439da931c50b0
Muhammad Rafi <murafi@cisco.com>
2022-03-26 11:30:03 +0000

    Bump version: 0.5.0 → 0.6.0

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ try:
     __author__ = """Muhammad Rafi"""
     __version__ = importlib.metadata.version(__package__ or __name__)
 except importlib.metadata.PackageNotFoundError:
-    __version__ = "0.5.0"
+    __version__ = "0.6.0"
